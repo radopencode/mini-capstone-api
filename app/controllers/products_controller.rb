88be863a4b02b07bb11b5f6e3ca085b468399a67
Ryan Day <radday1@gmail.com>
2022-03-09 14:01:30 -0700

    product update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def shop
+    better_than_amazon = Product.all
+    render json: better_than_amazon.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/better_than_amazon", controller: "products", action: "shop"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
