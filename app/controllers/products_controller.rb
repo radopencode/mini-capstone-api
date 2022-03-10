360fce138b44308469b297302e39323a594cdd51
Ryan Day <radday1@gmail.com>
2022-03-10 12:56:31 -0700

    updated routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,3 +4,13 @@ class ProductsController < ApplicationController
     render json: better_than_amazon.as_json
   end
 end
+
+def product_all
+  input_value = params
+  render json: 
+end
+def one_product
+  product_id = params["id"]
+  product = Product.find_by(id: product_id)
+  render json: product.as_json
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/one_product" => "products#one_product"
+  get "/one_product/:wildcard
 end
